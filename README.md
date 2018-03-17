17689836a0fa8e7cabf83ecba8083bc7d7776dd9
vladimir <smirnovv91@gmail.com>
2018-03-17 16:27:53 +0300

    released functions, updted readme

--- README.md
+++ README.md
@@ -1,6 +1,24 @@
 # Coursera Dump
 
-[TODO. There will be project description]
+The program exports information about 20 random courses on the Coursera to the xlsx file.
+
+# How to Install
+
+Python 3 should be already installed. Then use pip (or pip3 if there is a conflict with old Python 2 setup) to install dependencies:
+
+pip install -r requirements.txt # alternatively try pip3
+
+Remember, it is recommended to use virtualenv/venv for better isolation.
+
+# Quickstart
+
+The program must be run using the console.
+
+How to run:
+```bash
+$ python3 coursera.py
+```
+After finishing, you will see coursera_courses_info.xlsx file in current directory.
 
 # Project Goals
 
--- coursera.py
+++ coursera.py
@@ -1,11 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
+from openpyxl import Workbook
+from random import randint
 
-link = "https://www.coursera.org/sitemap~www~courses.xml"
+
+LINK = "https://www.coursera.org/sitemap~www~courses.xml"
+
 
 def get_courses_list():
-
-    response_xml_urls = requests.get(link).text
+    response_xml_urls = requests.get(LINK).text
     parse_object = BeautifulSoup(response_xml_urls, "lxml")
     urls_list = [
         url_with_tag.get_text() for url_with_tag in parse_object.findAll("loc")
@@ -13,32 +16,58 @@ def get_courses_list():
     return urls_list
 
 
-def get_course_info(link = "https://www.coursera.org/learn/programming-in-python"):
-    response = requests.get(link).text
-    parse_object = BeautifulSoup(response, 'html.parser')
-    x = parse_object.findAll("div", {"class": "rc-Language"})
+def get_course_info(course_url):
+    response = requests.get(course_url)
+    response.encoding = "UTF-8"
+    response = response.text
+    parse_object = BeautifulSoup(response, "html.parser")
 
-    print(x)
-    print(x[0].get_text())
+    course_name_tag = parse_object.findAll(
+        "h2", {"class": "headline-4-text course-title"}
+    )
+    course_language_tag = parse_object.findAll(
+        "div", {"class": "rc-Language"}
+    )
+    course_start_date_tag = parse_object.findAll(
+        "div", {"class": "startdate rc-StartDateString caption-text"}
+    )
+    course_rating_tag = parse_object.findAll(
+        "div", {"class": "ratings-text bt3-visible-xs"}
+    )
+    course_name = course_name_tag[0].get_text()
+    course_language = course_language_tag[0].get_text()
+    course_start_date = course_start_date_tag[0].get_text()
+    if course_rating_tag:
+        course_rating = course_rating_tag[0].get_text()
+    else:
+        course_rating = None
+    return (
+        course_name,
+        course_language,
+        course_start_date,
+        course_rating,
+        course_url
+    )
 
-    
 
-def output_courses_info_to_xlsx(filepath):
-    pass
+def output_courses_info_to_xlsx(data):
+    xlsx_book = Workbook()
+    xlsx_book_filename = "coursera_courses_info.xlsx"
+    xlsx_book_sheet = xlsx_book.active
+    xlsx_book_sheet.append(["Name", "Language", "Start date","Rating", "URL"])
+    for row in data:
+        xlsx_book_sheet.append(row)
+    xlsx_book.save(filename=xlsx_book_filename)
 
 
-if __name__ == '__main__':
-    x = get_courses_list()
-    #print(requests.get(x[0]).headers)
+if __name__ == "__main__":
+    count_courses_to_xlsx = 20
+    course_url_list = get_courses_list()
+    data_to_xlsx_export = []
+    for i in range(count_courses_to_xlsx):
+        random_number = randint(0, len(course_url_list))
+        data_to_xlsx_export.append(
+            get_course_info(course_url_list[random_number])
+        )
+    output_courses_info_to_xlsx(data_to_xlsx_export)
 
-    headers = {
-        'User-Agent': 'My User Agent 1.0',
-        'From': 'youremail@domain.com'
-    }
-    #print(x[0])
-    #response = requests.get(x[0], headers=headers)
-    #print(response.content)
-
-    #for i in x:
-    #   print(i)
-    get_course_info()
